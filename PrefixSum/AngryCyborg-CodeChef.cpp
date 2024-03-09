206082f95c83ff6bb2a3ff85f7baad982936881b
Moner Mohammed <orabimunnier@hotmail.com>
2024-03-09 23:04:32 +0200

    1D increasing prefix still trying

---
+++ PrefixSum/AngryCyborg-CodeChef.cpp
@@ -0,0 +1,27 @@
+//
+// Created by orabi on 3/9/2024.
+//
+#include <bits/stdc++.h>
+
+using namespace std ;
+
+int main(){
+    int T ;
+    cin >> T ;
+    while(T--){
+        int N ;
+        cin >> N ;
+
+        vector<int>input(N+1,0);
+        int Q ;
+        cin >> Q ;
+        while(Q--){
+            int l , r ;
+            cin >> l >> r ;
+            input[l] = 1 ;
+            if(r+1<=N) input[r+1] = -1 ;
+            for(int )
+        }
+
+    }
+}
