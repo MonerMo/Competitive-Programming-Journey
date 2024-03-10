f4cef353798da7fa2e751a2e107d35efd3808d7f
Moner Mohammed <orabimunnier@hotmail.com>
2024-03-10 11:20:38 +0200

    Trying on Angry Cyborg

--- PrefixSum/AngryCyborg-CodeChef.cpp
+++ PrefixSum/AngryCyborg-CodeChef.cpp
@@ -12,16 +12,34 @@ int main(){
         int N ;
         cin >> N ;
 
-        vector<int>input(N+1,0);
+        vector<int>output(N+1,0);
         int Q ;
         cin >> Q ;
         while(Q--){
+            vector<int>input(N+1,0);
+
             int l , r ;
             cin >> l >> r ;
             input[l] = 1 ;
-            if(r+1<=N) input[r+1] = -1 ;
-            for(int )
+            //if(r+1<=N) input[r+1] = -1 ;
+            for(int i = l+1 ; i <= r ; i++){
+                input[i] += input[i-1] ;
+            }
+
+
+            for(int i = l+1 ; i <= r ; i++){
+                input[i] += input[i-1] ;
+            }
+
+
+            for(int i = l ; i <= r ; i++){
+                output[i] += input[i] ;
+            }
         }
+
+        for(int i = 1 ; i <= N ; i++)
+            cout << output[i] << " " ;
+        cout << endl;
 
     }
 }
